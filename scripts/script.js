189d9fe479037f19236c0f753df7804598f1a589
Karinakazantseva <karinakazantseva18@gmail.com>
2025-04-02 09:53:36 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,28 @@
 'use strict'
 document.addEventListener("DOMContentLoaded", () => {
     console.log('Скрипт отработал корректно')
-
+    /*1. Появление форм*/
+    /* 
+        *   Алгоритм
+        *
+        *   1. Начало.
+        *   2. Получаем кнопку
+        *   3. Проверка условия: нажатие на кнопку.
+        *       3.1. Да: открыть форму
+        *           3.1.1 Проверка условия: нажатие на крестик для выхода
+        *               3.1.1.1. Да: закрыть форму
+        *               3.1.1.2. Нет: возвращаемся в "открыть форму"
+        *       3.2. Нет: Конец
+        *   4. Конец
+        * 
+        *   Блок-схема: /images/Block-schema.png
+        */
+    const signup = document.querySelectorAll(".signup");
+    signup.forEach((item, index) => {
+    item.addEventListener ('click', (event) => {
+    event.preventDefault();
+        console.log("Перейти");
+    });
+    });
 });
+
